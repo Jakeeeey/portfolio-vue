af178b9af41e4e5bcb8808198e3e5c94493d75b3
Jakeeeey <jakedavedeguzman123@gmail.com>
2025-03-19 21:41:10 +0800

    second commit

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { defineProps } from 'vue';
-//import bg from '@assets/images/background_image_code.mp4'
+import { RouterLink } from 'vue-router';
+import bg from '@/assets/images/background_image_code.mp4'
 import CTA from './CTA.vue';
 
 defineProps({
@@ -16,20 +17,33 @@ defineProps({
 </script>
 
 <template>
-  <div class="min-h-screen bg-sky-300 flex justify-center items-center">
-    <div class="container flex flex-col md:flex-row justify-center items-center gap-10">
-      <div class="basis-full flex flex-col justify-center items-center border border-red-400">
-        <video src="../assets/images/background_image_code.mp4"></video>
-        <h2 class="text-5xl">{{ heading }}</h2>
-        <h4 class="text-2xl">{{ subHeading }}</h4>
-        <div class="flex flex-col md:flex-row gap-5">
-          <CTA name="Contact" classType="px-4 py-2 font-medium bg-sky-500 hover:bg-white hover:text-black text-red-500 rounded-md"/>
-          <CTA name="Learn More" classType="px-4 py-2 font-medium hover:bg-white hover:text-black text-red-500 border rounded-md"/>
+  <section class="flex justify-center items-center py-24 relative min-h-screen">
+    <video :src="bg" autoplay loop muted
+            class="object-cover blur-sm grayscale h-full absolute left-0 top-0"></video>
+        <div class="container flex items-center py-5 flex-col-reverse xl:flex-row gap-20 z-50">
+            <div class="basis-full xl:basis-1/2 space-y-7">
+                <div>
+                    <h2 class="text-4xl 2xl:text-5xl text-white">Hi, I'm <span class="text-primary">Jake De Guzman</span>,
+                    </h2>
+                    <h1 class="text-7xl 2xl:text-8xl text-white">Bringing Ideas to Life Through Code.</h1>
+                </div>
+                <p class="text-lg text-white">An aspiring web developer specializing in crafting user-friendly and
+                    dynamic web
+                    solutions.</p>
+                <div class="flex justify-center items-center flex-col xl:flex-row gap-5 lg:gap-12">
+                    <RouterLink to="#"
+                        class="w-3/4 xl:w-1/4 py-3 font-medium px-5 border border-white rounded-lg text-center text-white hover:bg-gray-100 hover:text-gray-900 transition duration-150 ease-in-out transform hover:scale-110">Learn
+                        More</RouterLink>
+                    <RouterLink to="/contact"
+                        class="w-3/4 xl:w-1/4 py-3 font-medium px-5 bg-primary text-center text-white rounded-lg transition duration-150 ease-in-out transform hover:scale-110">Contact
+                        Me</RouterLink>
+                </div>
+            </div>
+            <div class="basis-full xl:basis-1/2 flex  justify-center">
+                <div
+                    class="justify-center size-96 xl:w-[500px] xl:h-[500px] 2xl:w-[600px] 2xl:h-[600px] rounded-full bg-center bg-cover bg-no-repeat bg-[url('../assets/images/wency.jpeg')]">
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="basis-full flex justify-center items-center border border-red-400">
-        <img class="size-96" src="../assets/images/wency.jpeg" alt="" />
-      </div>
-    </div>
-  </div>
+  </section>
 </template>
